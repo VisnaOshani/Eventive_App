170cee54f13a5dd8f72607e7075ec400c84f69b9
visnaOshani <jayasinghe548@gmail.com>
2019-08-18 23:23:57 +0530

    spinner for category

--- app/src/main/java/com/example/eventive/Budget_Details.java
+++ app/src/main/java/com/example/eventive/Budget_Details.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class Budget_Details extends AppCompatActivity {
 
@@ -12,6 +16,12 @@ public class Budget_Details extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_budget__details);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<String> adapter =  new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.category_arrays));
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
     public void saveBudgets(View view){
@@ -23,18 +33,6 @@ public class Budget_Details extends AppCompatActivity {
         Intent intent = new Intent(Budget_Details.this,Add_Budgets.class);
         startActivity(intent);
     }
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 
